db1a74cb2fb0cfbd7776cf3b3d9706d4b619e605
josephmckenna <joseph.mckenna@cern.ch>
2022-08-10 11:07:13 +0200

    Add 'edouble' class Simple class that behaves like a double and it will track errors. Construction from an integer will assume counting statistics

---
+++ ErrorCombination.h
@@ -0,0 +1,161 @@
+#ifndef _ERROR_COMBINATION_
+#define _ERROR_COMBINATION_
+
+#include <cmath>
+#include <cstdint>
+// Implementation of errors documented in
+// http://www.met.rdg.ac.uk/~swrhgnrj/combining_errors.pdf
+
+class edouble
+{
+public:
+   double value;
+   double error;
+
+public:
+   // Disable default constructor... the memory will is uninitialised
+   edouble() = delete;
+   // Only allow construction from integer types (integer infers counting statistics)
+   explicit edouble(int8_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(int16_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(int32_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(int64_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(uint8_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(uint16_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(uint32_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+   explicit edouble(uint64_t n)
+   {
+      value = n;
+      error = sqrt(value);
+   }
+
+   explicit edouble(double n, double e)
+   {
+      value = n;
+      error = e;
+   }
+   edouble(const edouble &e)
+   {
+      value = e.value;
+      error = e.error;
+   }
+
+   // Only allow construction from integer
+   // template <class T>
+   edouble(double) = delete;
+
+   // Combination functions
+   edouble operator+(const edouble &rhs) const
+   {
+      edouble temp(
+          (double)value + rhs.value,
+          (double)sqrt(error * error + rhs.error * rhs.error));
+      return temp;
+   }
+
+   edouble operator-(const edouble &rhs) const
+   {
+      edouble temp(
+          value - rhs.value,
+          sqrt(error * error + rhs.error * rhs.error));
+      return temp;
+   }
+
+   edouble operator*(const edouble &rhs) const
+   {
+      edouble temp(0.0, 0.0);
+
+      temp.value = value * rhs.value;
+
+      const double DeltaB_Over_B = error / value;
+      const double DeltaC_Over_C = rhs.error / rhs.value;
+
+      const double e = sqrt(DeltaB_Over_B * DeltaB_Over_B + DeltaC_Over_C * DeltaC_Over_C);
+      temp.error = temp.value * e;
+
+      return temp;
+   }
+
+   edouble operator/(const edouble &rhs) const
+   {
+      edouble temp(0.0, 0.0);
+
+      temp.value = value / rhs.value;
+
+      const double DeltaB_Over_B = error / value;
+      const double DeltaC_Over_C = rhs.error / rhs.value;
+
+      const double e = sqrt(DeltaB_Over_B * DeltaB_Over_B + DeltaC_Over_C * DeltaC_Over_C);
+      temp.error = temp.value * e;
+
+      return temp;
+   }
+
+   // Multiply by constant functions
+   edouble operator*(const double d) const
+   {
+      edouble ans(this->value, this->error);
+      ans.value *= d;
+      ans.error *= fabs(d);
+      return ans;
+   }
+
+   edouble operator/(const double d) const
+   {
+      edouble ans(this->value, this->error);
+      ans.value /= d;
+      ans.error /= d;
+      return ans;
+   }
+};
+
+edouble operator*(const double d, const edouble &rhs)
+{
+   // Multiply function is mutable, reuse code:
+   return rhs * d;
+}
+
+edouble operator/(const double d, const edouble &rhs)
+{
+   edouble temp(0.0, 0.0);
+   temp.value = d / rhs.value;
+   temp.error = fabs(d / (rhs.value * rhs.value)) * rhs.error;
+   return temp;
+}
+
+#include <ostream>
+std::ostream &operator<<(std::ostream &os, const edouble &rhs)
+{
+   os << "(" << rhs.value << " +/- " << rhs.error << ")";
+   return os;
+}
+
+#endif
---
+++ tests/SimpleExample.cxx
@@ -0,0 +1,39 @@
+#include "ErrorCombination.h"
+
+#include <iostream>
+
+int main()
+{
+    edouble a(4);
+    edouble b(9);
+
+    edouble c = a + b;
+    edouble d = a - b;
+    edouble e = a * b;
+    edouble f = a / b;
+    std::cout << a << " + " << b << " = " << c << std::endl;
+    std::cout << a << " - " << b << " = " << d << std::endl;
+    std::cout << a << " * " << b << " = " << e << std::endl;
+    std::cout << a << " / " << b << " = " << f << std::endl;
+
+    edouble g = a * 2.0;
+    edouble h = 2.0 * a;
+    std::cout << a << " * " << 2.0 << " = " << g << std::endl;
+    std::cout << 2.0 << " * " << a << " = " << h << std::endl;
+
+    edouble j = a / 2.0;
+    edouble k = 2.0 / a;
+    std::cout << a << " * " << 2.0 << " = " << j << std::endl;
+    std::cout << 2.0 << " / " << a << " = " << k << std::endl;
+
+    // Output:
+    //
+    // (4 +/- 2) + (9 +/- 3) = (13 +/- 3.60555)
+    // (4 +/- 2) - (9 +/- 3) = (-5 +/- 3.60555)
+    // (4 +/- 2) * (9 +/- 3) = (36 +/- 21.6333)
+    // (4 +/- 2) / (9 +/- 3) = (0.444444 +/- 0.267078)
+    // (4 +/- 2) * 2 = (8 +/- 4)
+    // (4 +/- 2) * 2 = (2 +/- 1)
+
+    return 0;
+}
